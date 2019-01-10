cd2fc48fa812eae36b008a6e8806cc06c44bab1b
Jesus <jesus.jg.medina@gmail.com>
2019-01-10 14:53:21 -0500

    Manual submit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
+  attr_reader :name
+  attr_accessor :status, :balance
+
+  def deposit(int)
+    @balance += int
+  end
+
+  def balance=(int)
+    @balance = int
+  end
+
+  def display_balance
+    puts "Your balance is $#{@balance}."
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    return @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+require "pry"
 class Transfer
-  # your code here
+  def initialize(sender, receiver, amount=0)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+
+  attr_reader :status, :sender, :receiver
+  attr_accessor :amount
+
+  def valid?
+    return sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+    if @status != "complete"
+      @sender.balance-= amount
+      @receiver.deposit(amount)
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance-= amount
+      @sender.deposit(amount)
+      @status = "reversed"
+    end
+  end
 end

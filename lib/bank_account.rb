2010a886be529cd36f518d58ebd8295cc6896a6a
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 06:31:39 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  attr_reader :name, :recever, :sender
+  attr_accessor :balance, :status
+
+  def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+  end
+
+  def deposit(value)
+    @balance += value
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    self.balance > 0 && self.status == 'open' ? true: false
+    # if self.balance > 0 && self.status == "open"
+    #   return true
+    # else
+    #   return false
+    # end
+
+    #asdfsadfasdf
+    # return self.balance > 0 && self.status == "open" ? true:false
+    # return  self.balance && self.status
+  end
+
+  def close_account
+      self.status = "closed"
+    #$$$$$
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,58 @@
+require 'pry'
+require_relative 'bank_account.rb'
+
 class Transfer
+  attr_reader :sender, :receiver, :status, :amount
   # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    return sender.valid? && receiver.valid?
+  end
+  #
+  def execute_transaction
+    if self.valid? && @status == "pending" && sender.balance - @amount  > 0
+      @receiver.deposit(@amount)
+      @sender.deposit(@amount * -1)
+      @status = "complete"
+    # elsif sender.valid? == false && receiver.valid? == true
+      # binding.pry
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+      #@@@@
+    end
+  end
+
+    def reverse_transfer
+      # execute_transaction
+      if @status == "complete"
+      @receiver.deposit(@amount * -1)
+      @sender.deposit(@amount)
+      @status = "reversed"
+      # binding.pry
+      end
+    end
+
+
+  # def reverse_tra
+  # nsfer
+  #   execute_transaction
+  #   if sender.valid?
+  #     @receiver -= @amount
+  #     @sender += @amount
+  #   end
+  # end
+
 end
+
+#
+# avi = BankAccount.new("Avi")
+# amanda = BankAccount.new("Amanda")
+# transfer = Transfer.new(amanda, avi, 50)
+# bad_transfer = Transfer.new(amanda, avi, 4000)
